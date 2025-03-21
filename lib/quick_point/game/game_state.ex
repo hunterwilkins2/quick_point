d7b4f3ecabccf18ad4eac46708582a40f1e19e8f
Hunter Wilkins <hunter.wilkins2@gmail.com>
2025-03-21 19:09:09 -0400

    track user presence with gamestate genserver

--- lib/quick_point/game/game_state.ex
+++ lib/quick_point/game/game_state.ex
@@ -12,6 +12,9 @@ defmodule QuickPoint.Game.GameState do
 
   def current_state(room_id), do: GenServer.call(process_name(room_id), :get_state)
 
+  def add_player(pid, room_id, user),
+    do: GenServer.cast(process_name(room_id), {:add_player, pid, user})
+
   def update_room(%Room{} = room), do: GenServer.cast(process_name(room.id), {:update_room, room})
 
   @impl true
@@ -25,8 +28,8 @@ defmodule QuickPoint.Game.GameState do
   end
 
   @impl true
-  def handle_cast({:update_room, %Room{} = room}, state) do
-    Logger.debug("Updating room name to #{room.name}", anasi_color: :blue)
+  def handle_cast({:update_room, %Room{} = room}, %Game{} = state) do
+    Logger.debug("Updating room name to #{room.name}", ansi_color: :blue)
 
     state = %Game{state | room: room}
     broadcast_state!(state)
@@ -35,11 +38,49 @@ defmodule QuickPoint.Game.GameState do
   end
 
   @impl true
-  def handle_call(:get_state, _from, state), do: {:reply, state, state}
+  def handle_cast({:add_player, pid, user}, %Game{} = state) do
+    Logger.debug("Added #{user.name} to #{state.room.id} from #{inspect(pid)}", ansi_color: :blue)
+    Process.monitor(pid)
+
+    state = %Game{
+      state
+      | users: Map.put(state.users, pid, user),
+        total_users: state.total_users + 1
+    }
+
+    broadcast_state!(state)
+
+    {:noreply, state}
+  end
 
   @impl true
-  def terminate(reason, state) do
-    Logger.info("GameState #{state.room_id} shutdown with reason: #{reason}", ansi_color: :yellow)
+  def handle_call(:get_state, _from, %Game{} = state), do: {:reply, state, state}
+
+  @impl true
+  def handle_info({:DOWN, _, :process, pid, reason}, %Game{} = state) do
+    Logger.debug("#{inspect(pid)} exited with #{inspect(reason)}", ansi_color: :blue)
+
+    Logger.debug("Removed #{Map.get(state.users, pid).name} from #{state.room.id}",
+      ansi_color: :blue
+    )
+
+    state = %Game{state | users: Map.delete(state.users, pid), total_users: state.total_users - 1}
+    broadcast_state!(state)
+
+    if Enum.count(state.users) == 0 do
+      Logger.info("No users in #{state.room.id}. Shutting down...", ansi_color: :yellow)
+      Process.exit(self(), :normal)
+    end
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info(_msg, state), do: {:noreply, state}
+
+  @impl true
+  def terminate(_reason, state) do
+    Logger.info("GameState #{state.room.id} shutdown", ansi_color: :yellow)
     state
   end
 
--- lib/quick_point_web/live/room_live/show.ex
+++ lib/quick_point_web/live/room_live/show.ex
@@ -12,6 +12,10 @@ defmodule QuickPointWeb.RoomLive.Show do
     QuickPoint.Game.Supervisor.start_child(id)
     Phoenix.PubSub.subscribe(QuickPoint.PubSub, "room:#{id}")
 
+    if connected?(socket) do
+      GameState.add_player(self(), id, socket.assigns.current_user)
+    end
+
     state = GameState.current_state(id)
 
     {:ok, update_state(socket, state)}
@@ -23,19 +27,19 @@ defmodule QuickPointWeb.RoomLive.Show do
   end
 
   @impl true
-  def handle_info({GameState, new_state}, socket) do
-    {:noreply, update_state(socket, new_state)}
-  end
+  def handle_info({GameState, new_state}, socket), do: {:noreply, update_state(socket, new_state)}
 
   defp update_state(socket, %Game{} = state) do
     users =
       state.users
-      |> Enum.map(fn {id, user} -> %{id: id, user: user, vote: Map.get(state.votes, id)} end)
+      |> Enum.map(fn {_, user} ->
+        %{id: user.id, user: user, vote: Map.get(state.votes, user.id)}
+      end)
 
     socket
     |> assign(:room, state.room)
     |> assign(:vote, Map.get(state.votes, socket.assigns.current_user.id))
     |> assign(:game_state, state.state)
-    |> stream(:users, users)
+    |> stream(:users, users, reset: true)
   end
 end
